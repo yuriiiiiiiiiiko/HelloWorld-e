ace405ce24b4998d3e3ae383152158f30179c429
g15901ay <g15901ay@s30431.tsuda.ac.jp>
2017-04-18 13:46:07 +0900

    Hello!!

---
+++ src/helloworld/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author g15901ay
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
